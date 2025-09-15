88036cff88d03a6489b4cb05f43b214ecbc09a8f
nasirmasud <nasir.masud@ymail.com>
2025-09-15 22:26:19 +0600

    Updated the concurrently package configuration

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <>
-      <h1>Hello</h1>
+      <h1>Hello from client</h1>
       <p>{message}</p>
     </>
   );
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -12,7 +12,7 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.get("/api/hello", (req: Request, res: Response) => {
-  res.json({ message: "Hello, World!" });
+  res.json({ message: "Hello, Server World!" });
 });
 
 app.listen(port, () => {
