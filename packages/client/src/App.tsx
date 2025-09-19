89b88f0057857befef1892e9423b388864fa35ee
nasirmasud <nasir.masud@ymail.com>
2025-09-19 15:48:38 +0600

    Setup shadCn Ui & add a button

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   const [message, setMessage] = useState("");
@@ -12,8 +13,11 @@ function App() {
 
   return (
     <>
-      <h1>Hello from client</h1>
-      <p className='font-bold p-4 text-3xl'>{message}</p>
+      <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
+        <h1>Hello from client</h1>
+        <p className='font-bold p-4 text-3xl'>{message}</p>
+        <Button variant='outline'>Click Me</Button>
+      </div>
     </>
   );
 }
